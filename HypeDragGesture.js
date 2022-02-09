c7a859e2295300541309176eef1167aa67b0e8db
Max Ziebell <1752114+worldoptimizer@users.noreply.github.com>
2022-02-09 13:56:39 +0100

    v1.0.0

--- HypeDragGesture.js
+++ HypeDragGesture.js
@@ -10,169 +10,160 @@ copyright (c) 2021 Max Ziebell, (https://maxziebell.de). MIT-license
 */
 if("HypeDragGesture" in window === false) window['HypeDragGesture'] = (function () {
 
-	function HypeDocumentLoad (hypeDocument, element, event) {
-		
-		/**
-		 * This function is to be called in an "On Drag" action.
-		 *
-		 * @param {String} hypeDocument forward the API
-		 * @param {HTMLDivElement} element forward the element
-		 * @param {Object} event forward the event
-		 */
-			hypeDocument.onDrag = function(hypeDocument, element, event){
+    /**
+     * This function is to be called in an "On Drag" action.
+     *
+     * @param {String} hypeDocument forward the API
+     * @param {HTMLDivElement} element forward the element
+     * @param {Object} event forward the event
+     */
+    function onDrag (hypeDocument, element, event){
 
-			//TODO drag group and resolve data-drag-group in 1.0.5
-			
-			// limit this Hype function to drag and drop
-			if (!event.hypeGesturePhase) return;
-			
-			// prep vars
-			var sceneElm = document.getElementById(hypeDocument.currentSceneId());
-			
-			// evalute base on drag phase
-			switch (event.hypeGesturePhase) {
-				
-				// start
-				case hypeDocument.kHypeGesturePhaseStart:
-					// if there is no data-intersecting attribute create one
-					if (!element.getAttribute('data-intersecting')) element.setAttribute('data-intersecting', 'false');
-			
-					// store position drag originated from
-					var dragStartTop = hypeDocument.getElementProperty(element, 'top');
-					var dragStartLeft = hypeDocument.getElementProperty(element, 'left');
-					element.setAttribute('data-drag-start-top', dragStartTop);
-					element.setAttribute('data-drag-start-left', dragStartLeft);
+        // limit this Hype function to drag and drop
+        if (!event.hypeGesturePhase) return;
+        
+        // prep vars
+        var sceneElm = document.getElementById(hypeDocument.currentSceneId());
+        
+        // evalute base on drag phase
+        switch (event.hypeGesturePhase) {
+            
+            // start
+            case hypeDocument.kHypeGesturePhaseStart:
+                // if there is no data-intersecting attribute create one
+                if (!element.getAttribute('data-intersecting')) element.setAttribute('data-intersecting', 'false');
+        
+                // store position drag originated from
+                var dragStartTop = hypeDocument.getElementProperty(element, 'top');
+                var dragStartLeft = hypeDocument.getElementProperty(element, 'left');
+                element.setAttribute('data-drag-start-top', dragStartTop);
+                element.setAttribute('data-drag-start-left', dragStartLeft);
 
-					// trigger actions found on the element in [data-drag-start-action]	and defaults
-					var expression = element.getAttribute('data-drag-start-action');
-					if (expression) hypeDocument.triggerAction (expression, {
-						element: element,
-						event: Object.assign({
-							dragStartTop: dragStartTop,
-							dragStartLeft: dragStartLeft,
-							dragGesturePhase: 'DragStart'
-						}, event)
-					});
+                // trigger actions found on the element in [data-drag-start-action]	and defaults
+                var expression = element.getAttribute('data-drag-start-action');
+                if (expression) hypeDocument.triggerAction (expression, {
+                    element: element,
+                    event: Object.assign({
+                        dragStartTop: dragStartTop,
+                        dragStartLeft: dragStartLeft,
+                        dragGesturePhase: 'DragStart'
+                    }, event)
+                });
 
-					break;
-					
-				// move
-				case hypeDocument.kHypeGesturePhaseMove:
-					// trigger actions found on the element in [data-drag-move-action] and defaults
-					var expression = element.getAttribute('data-drag-move-action');
-					if (expression) hypeDocument.triggerAction (expression, {
-						element: element,
-						event: Object.assign({
-							dragGesturePhase: 'DragMove'
-						}, event)
-					});
-					break;
-			}
+                break;
+                
+            // move
+            case hypeDocument.kHypeGesturePhaseMove:
+                // trigger actions found on the element in [data-drag-move-action] and defaults
+                var expression = element.getAttribute('data-drag-move-action');
+                if (expression) hypeDocument.triggerAction (expression, {
+                    element: element,
+                    event: Object.assign({
+                        dragGesturePhase: 'DragMove'
+                    }, event)
+                });
+                break;
+        }
 
-			// process intersections
-			
-			// query dropTargetElms
-			var intersectionElms;
-			var dropTargetElms = sceneElm.querySelectorAll(element.getAttribute('data-drop-selector'));
-			
-			if (dropTargetElms) {
-			
-				//query intersections
-				intersectionElms = hypeDocument.queryIntersections(element, dropTargetElms);
+        // process intersections
+        
+        // query dropTargetElms
+        var intersectionElms;
+        var dropTargetElms = sceneElm.querySelectorAll(element.getAttribute('data-drop-selector'));
+        
+        if (dropTargetElms) {
+        
+            //query intersections
+            intersectionElms = hypeDocument.queryIntersections(element, dropTargetElms);
 
-				// toggle data attribute data-intersecting
-				if (intersectionElms.length) {
+            // toggle data attribute data-intersecting
+            if (intersectionElms.length) {
 
-					// gate this trigger to case the state diverges
-					if (element.getAttribute('data-intersecting')!='true') {
+                // gate this trigger to case the state diverges
+                if (element.getAttribute('data-intersecting')!='true') {
 
-						// trigger  actions found on the element in [data-intersect-start-action] and defaults
-						var expression = element.getAttribute('data-intersection-start-action');
-						if (expression) hypeDocument.triggerAction (expression, {
-							element: element,
-							event: Object.assign({
-								intersectionElms: intersectionElms,
-								dragGesturePhase: 'IntersectionStart'
-							}, event)
-						});
-					}
+                    // trigger  actions found on the element in [data-intersect-start-action] and defaults
+                    var expression = element.getAttribute('data-intersection-start-action');
+                    if (expression) hypeDocument.triggerAction (expression, {
+                        element: element,
+                        event: Object.assign({
+                            intersectionElms: intersectionElms,
+                            dragGesturePhase: 'IntersectionStart'
+                        }, event)
+                    });
+                }
 
-				} else {
+            } else {
 
-					// gate this trigger to case the state diverges
-					if (element.getAttribute('data-intersecting')!='false') {
+                // gate this trigger to case the state diverges
+                if (element.getAttribute('data-intersecting')!='false') {
 
-						// trigger actions found on the element in [data-intersect-end-action] and defaults
-						var expression = element.getAttribute('data-intersection-end-action');
-						if (expression) hypeDocument.triggerAction (expression, {
-							element: element,
-							event: Object.assign({
-								dragGesturePhase: 'IntersectionEnd'
-							}, event)
-						});
-					}
+                    // trigger actions found on the element in [data-intersect-end-action] and defaults
+                    var expression = element.getAttribute('data-intersection-end-action');
+                    if (expression) hypeDocument.triggerAction (expression, {
+                        element: element,
+                        event: Object.assign({
+                            dragGesturePhase: 'IntersectionEnd'
+                        }, event)
+                    });
+                }
 
-				}
+            }
 
-				// remeber last intersection check
-				element.setAttribute('data-intersecting', intersectionElms.length? 'true' : 'false');
-			}
+            // remeber last intersection check
+            element.setAttribute('data-intersecting', intersectionElms.length? 'true' : 'false');
+        }
 
-			// process end and cancel gesture phase 
-			switch (event.hypeGesturePhase) {
-			
-				// end, cancel
-				case hypeDocument.kHypeGesturePhaseEnd:
-				case hypeDocument.kHypeGesturePhaseCancel:
-				
-					// trigger actions found on the element in [data-drag-end-action] and defaults
-					var expression = element.getAttribute('data-drag-end-action');
-					if (expression) hypeDocument.triggerAction (expression, {
-						element: element,
-						event: Object.assign({
-							dragGesturePhase: 'DragEnd'
-						}, event)
-					});
-		
-					if (dropTargetElms) {
-						// determine if our element being dragged intersects with a drop target
-						//intersectionElms = hypeDocument.queryIntersections(element, dropTargetElms);
-						if (intersectionElms.length) {
-						
-							// trigger actions found on the element in [data-drop-success-action] and defaults
-							var expression = element.getAttribute('data-drop-success-action');
-							if (expression) hypeDocument.triggerAction (expression, {
-								element: element,
-								event: Object.assign({
-									intersectionElms: intersectionElms,
-									dragGesturePhase: 'DropSuccess'
-								}, event)
-							});
-							
-						} else {
-							
-							// trigger actions found on the element in [data-drop-fail-action] and defaults
-							var expression = element.getAttribute('data-drop-fail-action');
-							if (expression) hypeDocument.triggerAction (expression, {
-								element: element,
-								event: Object.assign({
-									dragGesturePhase: 'DropFail'
-								}, event)
-							});
-						}
-					}
-				
-					// garbage collect intersecting on end, cancel
-					element.removeAttribute('data-intersecting');
+        // process end and cancel gesture phase 
+        switch (event.hypeGesturePhase) {
+        
+            // end, cancel
+            case hypeDocument.kHypeGesturePhaseEnd:
+            case hypeDocument.kHypeGesturePhaseCancel:
+            
+                // trigger actions found on the element in [data-drag-end-action] and defaults
+                var expression = element.getAttribute('data-drag-end-action');
+                if (expression) hypeDocument.triggerAction (expression, {
+                    element: element,
+                    event: Object.assign({
+                        dragGesturePhase: 'DragEnd'
+                    }, event)
+                });
+    
+                if (dropTargetElms) {
+                    // determine if our element being dragged intersects with a drop target
+                    //intersectionElms = hypeDocument.queryIntersections(element, dropTargetElms);
+                    if (intersectionElms.length) {
+                    
+                        // trigger actions found on the element in [data-drop-success-action] and defaults
+                        var expression = element.getAttribute('data-drop-success-action');
+                        if (expression) hypeDocument.triggerAction (expression, {
+                            element: element,
+                            event: Object.assign({
+                                intersectionElms: intersectionElms,
+                                dragGesturePhase: 'DropSuccess'
+                            }, event)
+                        });
+                        
+                    } else {
+                        
+                        // trigger actions found on the element in [data-drop-fail-action] and defaults
+                        var expression = element.getAttribute('data-drop-fail-action');
+                        if (expression) hypeDocument.triggerAction (expression, {
+                            element: element,
+                            event: Object.assign({
+                                dragGesturePhase: 'DropFail'
+                            }, event)
+                        });
+                    }
+                }
+            
+                // garbage collect intersecting on end, cancel
+                element.removeAttribute('data-intersecting');
 
-					break;
-			}	
-		}
-	}
-
-	/* setup callbacks */
-	if("HYPE_eventListeners" in window === false) { window.HYPE_eventListeners = Array();}
-	window.HYPE_eventListeners.push({"type":"HypeDocumentLoad", "callback": HypeDocumentLoad});
+                break;
+        }	
+    }
 
 	/**
 	 * @typedef {Object} HypeDragGesture
@@ -181,6 +172,7 @@ if("HypeDragGesture" in window === false) window['HypeDragGesture'] = (function 
 	 */
 	var HypeDragGesture = {
 		version: '1.0.0',
+		onDrag: onDrag,
 	};
 
 	/** 
