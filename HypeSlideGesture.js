77e9e05f4b3d2aebb68ec5580e94814602aff93b
Max Ziebell <1752114+worldoptimizer@users.noreply.github.com>
2022-02-10 15:07:42 +0100

    v1.0.0

--- HypeSlideGesture.js
+++ HypeSlideGesture.js
@@ -11,148 +11,148 @@ if("HypeSlideGesture" in window === false) window['HypeSlideGesture'] = (functio
 
 	function continueAfterDrag (hypeDocument, element, event, config){
 			
-        // limit this Hype function to drag and drop
-        if (!(event && event.hypeGesturePhase)) return;
+		// limit this Hype function to drag and drop
+		if (!(event && event.hypeGesturePhase)) return;
 
-        //gate ondrag
-        config = config || {};
-        
-        // prep vars based on config
-        var instance =  config.symbolInstance || hypeDocument; //autodetect?!
-        var timelineName = config.timelineName || 'timelineName'; 
-        var borderMode = config.borderMode || 'bounce'; // default?
-        var ms = config.ms || 100;
-        var maxVelocityTick =  (Math.abs(config.maxVelocity) || 12) / 60;
-        var friction = config.friction || 0.95;
-        var borderFriction = config.borderFriction || 0.1;
-        var treshold = config.treshold || 0.01;
-        var time = instance.currentTimeInTimelineNamed(timelineName);
-        var duration = instance.durationForTimelineNamed(timelineName);
-        
-        // switch based on event.eventGesturePhase
-        switch(event.hypeGesturePhase) {
+		//gate ondrag
+		config = config || {};
+		
+		// prep vars based on config
+		var instance =  config.symbolInstance || hypeDocument; //autodetect?!
+		var timelineName = config.timelineName || 'timelineName'; 
+		var borderMode = config.borderMode || 'bounce'; // default?
+		var ms = config.ms || 100;
+		var maxVelocityTick =  (Math.abs(config.maxVelocity) || 12) / 60;
+		var friction = config.friction || 0.95;
+		var borderFriction = config.borderFriction || 0.1;
+		var treshold = config.treshold || 0.01;
+		var time = instance.currentTimeInTimelineNamed(timelineName);
+		var duration = instance.durationForTimelineNamed(timelineName);
+		
+		// switch based on event.eventGesturePhase
+		switch(event.hypeGesturePhase) {
 
-            // start
-            case hypeDocument.kHypeGesturePhaseStart:
-                instance.isDragging = true;	
-                instance.dragTime = time;
-                clearInterval(instance.interval);
-                instance.interval = setInterval(function(){
-                    instance.dragTime = instance.currentTimeInTimelineNamed(timelineName);
-                }, ms);
+			// start
+			case hypeDocument.kHypeGesturePhaseStart:
+				instance.isDragging = true;	
+				instance.dragTime = time;
+				clearInterval(instance.interval);
+				instance.interval = setInterval(function(){
+					instance.dragTime = instance.currentTimeInTimelineNamed(timelineName);
+				}, ms);
 
-                var expression = element.getAttribute('data-timeline-drag-start-action');
-                if (expression) hypeDocument.triggerAction (expression, {
-                    element: element,
-                    event: Object.assign({
-                        instance: instance,
-                        dragGesturePhase: 'TimelineDragStart'
-                    }, event)
-                });
-                break;
+				var expression = element.getAttribute('data-timeline-drag-start-action');
+				if (expression) hypeDocument.triggerAction (expression, {
+					element: element,
+					event: Object.assign({
+						instance: instance,
+						dragGesturePhase: 'TimelineDragStart'
+					}, event)
+				});
+				break;
 
-            // end, cancel
-            case hypeDocument.kHypeGesturePhaseEnd:
-            case hypeDocument.kHypeGesturePhaseCancel:
-                instance.isDragging = false;
-                instance.dragDiff = Math.min(Math.max(time - instance.dragTime, -maxVelocityTick), maxVelocityTick);
-                clearInterval(instance.interval);
-                instance.interval = setInterval(function(){
-                    var time = instance.currentTimeInTimelineNamed(timelineName);
-                    
-                    if (time + instance.dragDiff > duration) {
-                        var overshot = time + instance.dragDiff - duration;
-                        switch(borderMode) {
-                            case 'bounce':
-                                instance.goToTimeInTimelineNamed(duration - overshot*borderFriction, timelineName);
-                                instance.dragDiff *= borderFriction;
-                                instance.dragDiff *= -1;
-                                break;
-                            
-                            case 'shift':
-                                instance.goToTimeInTimelineNamed(overshot, timelineName);
-                                break;
-                                
-                            case 'none':
-                            default:
-                                instance.goToTimeInTimelineNamed(duration, timelineName);
-                                instance.dragDiff = 0;
-                                break;
-                        }
-                    } else if (time + instance.dragDiff < 0){
-                        var overshot = time + instance.dragDiff;
-                        switch(borderMode) {
-                            case 'bounce':
-                                instance.goToTimeInTimelineNamed(-overshot*borderFriction, timelineName);
-                                instance.dragDiff *= borderFriction;
-                                instance.dragDiff *= -1;
-                                break;
-                            
-                            case 'shift':
-                                instance.goToTimeInTimelineNamed(duration-overshot, timelineName);
-                                break;
-                                
-                            case 'none':
-                            default:
-                                instance.goToTimeInTimelineNamed(0, timelineName);
-                                instance.dragDiff = 0;
-                                break;
-                        }
-                    } else {
-                        instance.goToTimeInTimelineNamed(Math.round((time + instance.dragDiff)*100)/100, timelineName);
-                    }
-                    
-                    instance.dragDiff *= friction;
-                    if (Math.abs(instance.dragDiff) < treshold ) {
-                        instance.pauseTimelineNamed(timelineName)
-                        clearInterval(instance.interval);
+			// end, cancel
+			case hypeDocument.kHypeGesturePhaseEnd:
+			case hypeDocument.kHypeGesturePhaseCancel:
+				instance.isDragging = false;
+				instance.dragDiff = Math.min(Math.max(time - instance.dragTime, -maxVelocityTick), maxVelocityTick);
+				clearInterval(instance.interval);
+				instance.interval = setInterval(function(){
+					var time = instance.currentTimeInTimelineNamed(timelineName);
+					
+					if (time + instance.dragDiff > duration) {
+						var overshot = time + instance.dragDiff - duration;
+						switch(borderMode) {
+							case 'bounce':
+								instance.goToTimeInTimelineNamed(duration - overshot*borderFriction, timelineName);
+								instance.dragDiff *= borderFriction;
+								instance.dragDiff *= -1;
+								break;
+							
+							case 'shift':
+								instance.goToTimeInTimelineNamed(overshot, timelineName);
+								break;
+								
+							case 'none':
+							default:
+								instance.goToTimeInTimelineNamed(duration, timelineName);
+								instance.dragDiff = 0;
+								break;
+						}
+					} else if (time + instance.dragDiff < 0){
+						var overshot = time + instance.dragDiff;
+						switch(borderMode) {
+							case 'bounce':
+								instance.goToTimeInTimelineNamed(-overshot*borderFriction, timelineName);
+								instance.dragDiff *= borderFriction;
+								instance.dragDiff *= -1;
+								break;
+							
+							case 'shift':
+								instance.goToTimeInTimelineNamed(duration-overshot, timelineName);
+								break;
+								
+							case 'none':
+							default:
+								instance.goToTimeInTimelineNamed(0, timelineName);
+								instance.dragDiff = 0;
+								break;
+						}
+					} else {
+						instance.goToTimeInTimelineNamed(Math.round((time + instance.dragDiff)*100)/100, timelineName);
+					}
+					
+					instance.dragDiff *= friction;
+					if (Math.abs(instance.dragDiff) < treshold ) {
+						instance.pauseTimelineNamed(timelineName)
+						clearInterval(instance.interval);
 
-                        var expression = element.getAttribute('data-timeline-slide-end-action');
-                        if (expression) hypeDocument.triggerAction (expression, {
-                            element: element,
-                            event: Object.assign({
-                                instance: instance,
-                                dragGesturePhase: 'TimelineSlideEnd'
-                            }, event)
-                        });
-                        
-                    } else {
+						var expression = element.getAttribute('data-timeline-slide-end-action');
+						if (expression) hypeDocument.triggerAction (expression, {
+							element: element,
+							event: Object.assign({
+								instance: instance,
+								dragGesturePhase: 'TimelineSlideEnd'
+							}, event)
+						});
+						
+					} else {
 
-                        var expression = element.getAttribute('data-timeline-slide-move-action');
-                        if (expression) hypeDocument.triggerAction (expression, {
-                            element: element,
-                            event: Object.assign({
-                                instance: instance,
-                                dragGesturePhase: 'TimelineSlideMove'
-                            }, event)
-                        });
-                        
-                    }
-                },1000/60);
+						var expression = element.getAttribute('data-timeline-slide-move-action');
+						if (expression) hypeDocument.triggerAction (expression, {
+							element: element,
+							event: Object.assign({
+								instance: instance,
+								dragGesturePhase: 'TimelineSlideMove'
+							}, event)
+						});
+						
+					}
+				},1000/60);
 
-                var expression = element.getAttribute('data-timeline-drag-end-action');
-                if (expression) hypeDocument.triggerAction (expression, {
-                    element: element,
-                    event: Object.assign({
-                        instance: instance,
-                        dragGesturePhase: 'TimelineDragEnd'
-                    }, event)
-                });
-                break;
-            
-            case hypeDocument.kHypeGesturePhaseMove:
+				var expression = element.getAttribute('data-timeline-drag-end-action');
+				if (expression) hypeDocument.triggerAction (expression, {
+					element: element,
+					event: Object.assign({
+						instance: instance,
+						dragGesturePhase: 'TimelineDragEnd'
+					}, event)
+				});
+				break;
+			
+			case hypeDocument.kHypeGesturePhaseMove:
 
-                var expression = element.getAttribute('data-timeline-drag-move-action');
-                if (expression) hypeDocument.triggerAction (expression, {
-                    element: element,
-                    event: Object.assign({
-                        instance: instance,
-                        dragGesturePhase: 'TimelineDragMove'
-                    }, event)
-                });
-                break;
-        }
-    }
+				var expression = element.getAttribute('data-timeline-drag-move-action');
+				if (expression) hypeDocument.triggerAction (expression, {
+					element: element,
+					event: Object.assign({
+						instance: instance,
+						dragGesturePhase: 'TimelineDragMove'
+					}, event)
+				});
+				break;
+		}
+	}
 
 	/**
 	 * @typedef {Object} HypeSlideGesture
@@ -161,7 +161,7 @@ if("HypeSlideGesture" in window === false) window['HypeSlideGesture'] = (functio
 	 */
 	var HypeSlideGesture = {
 		version: '1.0.0',
-        continueAfterDrag: continueAfterDrag,
+		continueAfterDrag: continueAfterDrag,
 	};
 
 	/** 
